eb85af860724f6e64a21b56763c9a32b280d8eec
Miran <Emilbekov.m091@mail.ru>
2023-11-16 17:23:09 +0600

    completing a task

--- task-1/src/App.js
+++ task-1/src/App.js
@@ -1,79 +1,40 @@
-// import logo from './logo.svg';
-// import './App.css';
-// // Декларативный стиль: описание структуры компонента React.
-// function App() {
-//   // Императивный стиль: прямые манипуляции с объектом Date.
-//   const currentYear = new Date().getFullYear(); 
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//         {/* Декларативный стиль: использование переменной currentYear в JSX. */}
-//         <p>{currentYear}</p>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-import logotip from './logo.svg';
+import { createElement } from 'react';
+import logo from './logo.svg';
 import './App.css';
+// Декларативный стиль: описание структуры компонента React.
 function App() {
+  // Императивный стиль: прямые манипуляции с объектом Date.
+  const currentYear = new Date().getFullYear(); 
+  return (
+    createElement(
+      'div', 
+      { className : 'App' }, 
+      createElement('header',
+      { className : 'App-header' },
+        createElement('img',
+        { src: logo, className: 'App-logo', alt: 'logo' }, ),
 
-  const currentYear = new Date().getFullYear();
-  // Создание элементов
-  let appDiv = document.createElement('div');
-  appDiv.className = 'App';
+        createElement('p',
+        null,
+        'Edit ',
+        createElement('code', null, 'src/App.js',),
+        ' and save to reload.',
+        ),
 
-  let header = document.createElement('header');
-  header.className = 'App-header';
-
-  let logo = document.createElement('img');
-  logo.src = logotip;
-  logo.className = 'App-logo';
-  logo.alt = 'logo';
-
-  let paragraph = document.createElement('p');
-  paragraph.textContent = 'Edit ';
-
-  let codeElement = document.createElement('code');
-  codeElement.textContent = 'src/App.js';
-
-  paragraph.appendChild(codeElement);
-  paragraph.textContent += ' and save to reload.';
-
-  let link = document.createElement('a');
-  link.className = 'App-link';
-  link.href = 'https://reactjs.org';
-  link.target = '_blank';
-  link.rel = 'noopener noreferrer';
-  link.textContent = 'Learn React';
-
-
-  let paragraphYear = document.createElement('p');
-  paragraphYear.textContent = currentYear;
-  
-  // Структурирование элементов
-  header.appendChild(logo);
-  header.appendChild(paragraph);
-  header.appendChild(link);
-  header.appendChild(paragraphYear);
-  appDiv.appendChild(header);
-
-  // Добавление созданных элементов на страницу
-  document.getElementById('root').appendChild(appDiv);
-  return
+        createElement('a', 
+        {className:'App-link', 
+        href: 'https://reactjs.org',
+        target: '_blank',
+        rel: 'noopener noreferrer'},
+        'Learn React',
+        ),
+        createElement('p',
+        null,
+        currentYear
+        ),
+        /* Декларативный стиль: использование переменной currentYear в JSX. */
+      )
+    )   
+  )    
 }
-// Вызов функции для создания приложения
 export default App;
